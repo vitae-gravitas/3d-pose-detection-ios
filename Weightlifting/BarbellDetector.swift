1088fca2ea39e35eaf7fa60b783b18e26724660f
Avinash Jain <avinashkalpitjain@gmail.com>
2019-07-12 00:40:45 -0700

    Updated object detection with better UI, still needs work with rotation and fixing onto object

---
+++ Weightlifting/BarbellDetector.swift
@@ -0,0 +1,53 @@
+//
+//  BarbellDetector.swift
+//  Weightlifting
+//
+//  Created by Avinash Jain on 7/5/19.
+//  Copyright © 2019 Avinash Jain. All rights reserved.
+//
+
+import CoreML
+import Vision
+
+public class BarbellDetector {
+    // MARK: - Variables
+    private let visionQueue = DispatchQueue(label: "com.viseo.ARML.visionqueue")
+    
+    private lazy var predictionRequest: VNCoreMLRequest = {
+        // Load the ML model through its generated class and create a Vision request for it.
+        do {
+            let model = try VNCoreMLModel(for: BarbellModel().model)
+            let request = VNCoreMLRequest(model: model)
+            
+            // This setting determines if images are scaled or cropped to fit our 224x224 input size. Here we try scaleFill so we don't cut part of the image.
+            request.imageCropAndScaleOption = VNImageCropAndScaleOption.scaleFill
+            return request
+        } catch {
+            fatalError("can't load Vision ML model: \(error)")
+        }
+    }()
+    
+    // MARK: - Public functions
+    public func performDetection(inputBuffer: CVPixelBuffer, completion: @escaping (_ outputObservation: [VNRecognizedObjectObservation]?, _ error: Error?) -> Void) {
+        // Right orientation because the pixel data for image captured by an iOS device is encoded in the camera sensor's native landscape orientation
+        let requestHandler = VNImageRequestHandler(cvPixelBuffer: inputBuffer, orientation: .right)
+        
+        // We perform our CoreML Requests asynchronously.
+        visionQueue.async {
+            // Run our CoreML Request
+            do {
+                try requestHandler.perform([self.predictionRequest])
+                
+                guard let observations = self.predictionRequest.results as! [VNRecognizedObjectObservation]? else {
+                    completion(nil, nil)
+                    throw NSError()
+                }
+                
+                // The resulting image (mask) is available as observation.pixelBuffer
+                completion(observations, nil)
+            } catch {
+                completion(nil, error)
+            }
+        }
+    }
+}
--- Weightlifting/ViewController.swift
+++ Weightlifting/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 import RealityKit
 import ARKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var previewView: UIView!
     
+    var rootLayer: CALayer! = nil
+    var detectionOverlay: CALayer! = nil
     // The 3D character to display.
     var character: BodyTrackedEntity?
     let characterOffset: SIMD3<Float> = [-1.0, 0, 0] // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
+    
+    var detector = BarbellDetector()
+    
+    var bufferWidth:Float = 0.0
+    var bufferHeight:Float = 0.0
+    
+    var rootLayerHasLoaded = false
+    
+    var frameCount = 0
     
     // Sets up BodyPartManager with what body part types to track
     var bodyPartManager = BodyPartManager(with: [.leftHip, .leftKnee, .rightHip, .rightKnee])
@@ -25,7 +39,7 @@ class ViewController: UIViewController, ARSessionDelegate {
     // Threshold for the difference between
     let thresholdLegs:Float = 0.12
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         arView.session.delegate = self
         
@@ -57,6 +71,43 @@ class ViewController: UIViewController, ARSessionDelegate {
             }
         })
     }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        frameCount += 1
+        if frameCount % 10 == 0 {
+            frameCount = 1
+            let buffer = frame.capturedImage
+            
+            self.bufferHeight = Float(CVPixelBufferGetWidth(buffer))
+            self.bufferWidth  = Float(CVPixelBufferGetHeight(buffer))
+            
+            if rootLayerHasLoaded == false {
+                self.loadRootLayer()
+                self.loadDetectionOverlay()
+                self.updateLayerGeometry()
+            }
+            
+            
+            
+            print(self.bufferWidth, self.bufferHeight)
+            
+            detector.performDetection(inputBuffer: buffer, completion: {(obs, error) -> Void in
+                guard let observations = obs else {
+                    //                    CATransaction.begin()
+                    //                    CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+                    //                    self.detectionOverlay.sublayers = nil
+                    //                    CATransaction.commit()
+                    return
+                    
+                }
+                
+                self.generateBoundingBox(observations: observations)
+                
+            })
+            
+        }
+    }
+    
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         
@@ -93,3 +144,112 @@ class ViewController: UIViewController, ARSessionDelegate {
         }
     }
 }
+
+// MARK:- UI Code to render bounding box
+
+extension ViewController {
+    
+    func generateBoundingBox(observations: [VNRecognizedObjectObservation]) {
+        
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        detectionOverlay.sublayers = nil
+        
+        for observation in observations where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            
+            if topLabelObservation.confidence > 0.8 {
+                
+                let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferWidth), Int(bufferHeight))
+                
+                let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+                
+                let textLayer = self.createTextSubLayerInBounds(objectBounds,
+                                                                identifier: topLabelObservation.identifier,
+                                                                confidence: topLabelObservation.confidence)
+                shapeLayer.addSublayer(textLayer)
+                detectionOverlay.addSublayer(shapeLayer)
+            }
+        }
+        self.updateLayerGeometry()
+        CATransaction.commit()
+    }
+    
+    func loadRootLayer() {
+        rootLayer = previewView.layer
+        rootLayerHasLoaded = true
+    }
+    
+    func loadDetectionOverlay() {
+        detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+        detectionOverlay.name = "DetectionOverlay"
+        detectionOverlay.bounds = CGRect(x: 0.0,
+                                         y: 0.0,
+                                         width: Double(bufferWidth),
+                                         height: Double(bufferHeight))
+        detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
+        rootLayer.addSublayer(detectionOverlay)
+    }
+    
+    func updateLayerGeometry() {
+        let bounds = rootLayer.bounds
+        var scale: CGFloat
+        
+        let xScale: CGFloat = bounds.size.width / CGFloat(bufferHeight)
+        let yScale: CGFloat = bounds.size.height / CGFloat(bufferWidth)
+        
+        scale = fmax(xScale, yScale)
+        if scale.isInfinite {
+            scale = 1.0
+        }
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        
+        // rotate the layer into screen orientation and scale and mirror
+        
+        detectionOverlay.setAffineTransform(
+            CGAffineTransform(scaleX: scale, y: -scale)
+            //CGAffineTransform(rotationAngle: CGFloat(.pi * 5.0 / 2.0)).scaledBy(x: scale, y: -scale)
+        )
+        // center the layer
+        detectionOverlay.position = CGPoint (x: bounds.midX, y: bounds.midY)
+        
+        CATransaction.commit()
+    }
+    
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        let textLayer = CATextLayer()
+        textLayer.name = "Object Label"
+        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
+        let largeFont = UIFont(name: "Helvetica", size: 24.0)!
+        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
+        textLayer.string = formattedString
+        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
+        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        textLayer.shadowOpacity = 0.7
+        textLayer.shadowOffset = CGSize(width: 2, height: 2)
+        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
+        textLayer.contentsScale = 2.0 // retina rendering
+        // rotate the layer into screen orientation and scale and mirror
+        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
+        return textLayer
+    }
+    
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        print(bounds)
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
+    }
+}
+
+
